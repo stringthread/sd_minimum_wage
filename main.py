5ce594420fa96a9ff474e8f0e9506216888efe72
stringthread <string.thread@gmail.com>
2021-04-22 20:10:36 +0900

    final for presentation

--- main.py
+++ main.py
@@ -1,66 +1,172 @@
-TIME_LENGTH=120
+from copy import deepcopy
+TIME_LENGTH=80
 WAGE_STEP=100
-min_wage=1300#最低賃金
-min_liv_wage=1100#最低生計費
-wage_l=[i for i in range(500,2001,WAGE_STEP)]#時給:階級上限値
-worker_l=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#雇用者数
-job_l=[0,0,0,0,0,18,30,28,7,5,3,4,1,0,1,3]#求人
-unemp_n=70
-unemp_l=0
+rate_match=0.13#ミスマッチ指数: job=∞でt=30での失業率3%となるよう調整
+min_wage=1013#最低賃金
+min_liv_wage=1220000/12/173.8*1013/902#最低生計費
+initial_unemp_n=[340,300]
+unemp_n=deepcopy(initial_unemp_n)#新規就労希望者 [低求人,高求人]
+unemp_e=[0,0]#経験者失業
+wage=[i for i in range(600,2001,WAGE_STEP)]#時給:階級上限値
+worker=[
+    [[0,0],[0,0]], #-600
+    [[0,0],[0,0]], #-700
+    [[0,0],[0,0]], #-800
+    [[0,0],[0,0]], #-900
+    [[0,0],[0,0]], #-1000
+    [[0,0],[0,0]], #-1100
+    [[0,0],[0,0]], #-1200
+    [[0,0],[0,0]], #-1300
+    [[0,0],[0,0]], #-1400
+    [[0,0],[0,0]], #-1500
+    [[0,0],[0,0]], #-1600
+    [[0,0],[0,0]], #-1700
+    [[0,0],[0,0]], #-1800
+    [[0,0],[0,0]], #-1900
+    [[0,0],[0,0]], #1901-
+]#雇用者数: [時給階層][低求人/高求人][フルタイム/パートタイム]
+job=[
+    [0,0], #-600
+    [0,0], #-700
+    [0,0], #-800
+    [0,0], #-900
+    [18,18], #-1000
+    [30,30], #-1100
+    [28,28], #-1200
+    [7,7], #-1300
+    [5,5], #-1400
+    [3,3], #-1500
+    [4,4], #-1600
+    [1,1], #-1700
+    [0,0], #-1800
+    [1,1], #-1900
+    [323,423] #1901-
+]#求人: [時給階層][低求人/高求人]: フルタイム換算
+rate_part=[
+    [1,1], #-600
+    [1,1], #-700
+    [1,1], #-800
+    [1,1], #-900
+    [1,1], #-1000
+    [0.8,0.8], #-1100
+    [0.7,0.7], #-1200
+    [0.7,0.7], #-1300
+    [0.5,0.5], #-1400
+    [0.5,0.5], #-1500
+    [0.5,0.5], #-1600
+    [0.3,0.3], #-1700
+    [0.2,0.2], #-1800
+    [0.1,0.1], #-1900
+    [0.05,0.05] #1901-
+]
+worktime=[1,0.53]#[フル/パート]
 history_unemp=[-1 for i in range(TIME_LENGTH)]
 history_poor=[-1 for i in range(TIME_LENGTH)]
 def update_min_wage(t):
     global min_wage
-    if t>=60:
-        min_wage=900
+    if t>=30: #この辺りで諸数値が安定する
+        min_wage=1200
 
 def update_unemp():
-    global unemp_n,unemp_l,worker_l
-    for i in range(len(worker_l)):
-        unemp_l+=worker_l[i]*0.05
-        worker_l[i]=worker_l[i]*0.9
-    unemp_n+=10
+    global unemp_n,unemp_e,worker
+    for type in range(len(worker[0])):
+        for i in range(len(worker)):
+            for full_part in range(len(worker[0][0])):
+                if i==len(worker)-1: #主に正規雇用
+                    rate_change=0.01
+                    rate_retire=0.025 #40年で入れ替わり
+                else:
+                    rate_change=0.1
+                    rate_retire=0.025
+                unemp_e[type]+=worker[i][type][full_part]*rate_change#転職
+                worker[i][type][full_part]=worker[i][type][full_part]*(1-rate_change-rate_retire)#退職
+    #非就業者のretire
+    rate_retire=0.025
+    unemp_e[0]*=(1-rate_retire)
+    unemp_e[1]*=(1-rate_retire)
+    unemp_n[0]*=(1-rate_retire)
+    unemp_n[1]*=(1-rate_retire)
+    unemp_n[0]+=initial_unemp_n[0]*rate_retire
+    unemp_n[1]+=initial_unemp_n[1]*rate_retire
 
 def update_job():
     pass
 
+#return [n_full,n_part]
+def _n_job(i,type):
+    n_emp_total=job[i][type]*min((wage[i]-WAGE_STEP/2)/min_wage,1) #階級値/最低賃金だけ最低賃金で雇用
+    rp=rate_part[i][type] #パート比率
+    n_job=[(1-rp)/(1-(worktime[0]-worktime[1])*rp) * n_emp_total,
+           rp/(1-(worktime[0]-worktime[1])*rp) * n_emp_total] #人が足りている場合
+    sum_n_job=n_job[0]+n_job[1]
+    sum_worker=worker[i][type][0]+worker[i][type][1]
+    sum_employable=rate_match*(unemp_e[type]+unemp_n[type])
+    for full_part in range(2):
+        n_job[full_part]*=min((sum_worker+sum_employable)/sum_n_job, 1) if sum_n_job>0 else 1
+        #人手不足の調整: rate_part一定でn_jobを小さく
+    return n_job
+
 def update_worker():
-    global worker_l,job_l,unemp_n,unemp_l
-    n_under_min_wage=0
-    for i in range(len(worker_l)-1,-1,-1):
-        if(wage_l[i]<min_wage):
-            n_under_min_wage+=job_l[i]*min(1,wage_l[i]/min_wage)-worker_l[i]
-            continue
-        n_emp=job_l[i]*min(1,wage_l[i]/min_wage)-worker_l[i]+n_under_min_wage
-        n_under_min_wage=0
-        n_exp=min(n_emp,unemp_l*0.8)
-        unemp_l-=n_exp
-        worker_l[i]+=n_exp
-        if(n_emp==n_exp):
-            continue
-        n_new=min(n_emp-n_exp,unemp_n)
-        unemp_n-=n_new
-        worker_l[i]+=n_new
-        if(unemp_l<=0 and unemp_n<=0):
-            break
+    global worker,job,unemp_n,unemp_e
+    for type in range(len(worker[0])):
+        for i in range(len(worker)-1,-1,-1):
+            if(wage[i]<=min_wage):
+                break
+            n_under_min_wage=[0,0]
+            if(wage[i-1]<=min_wage):
+                for j in range(0,i):
+                    for full_part in range(len(worker[0][0])):
+                        unemp_e[type]+=worker[j][type][full_part]
+                        worker[j][type][full_part]=0
+                        n_under_min_wage[full_part]+=_n_job(j,type)[full_part]
+            n_job=_n_job(i,type)
+            for full_part in range(len(worker[0][0])):
+                n_emp=n_job[full_part] - worker[i][type][full_part] + n_under_min_wage[full_part]
+                if n_emp>=0:
+                    n_exp=min(n_emp*0.8,unemp_e[type]*rate_match) #最低2割は新人
+                    unemp_e[type]-=n_exp
+                    worker[i][type][full_part]+=n_exp
+                    if(n_emp==n_exp):
+                        continue
+                    n_new=min(n_emp-n_exp,unemp_n[type]*rate_match)
+                    unemp_n[type]-=n_new
+                    worker[i][type][full_part]+=n_new
+                    if(unemp_e[type]<=0 and unemp_n[type]<=0):
+                        break
+                else:
+                    n_emp*=-1
+                    worker[i][type][full_part]-=n_emp
+                    unemp_e[type]+=n_emp
 
 def save_data(t):
-    global history_poor,history_unemp
-    history_unemp[t]=unemp_n+unemp_l
-    history_poor[t]=history_unemp[t]
-    for i in range(len(wage_l)):
-        if wage_l[i]>min_liv_wage:
-            history_poor[t]+=worker_l[i]*(min_liv_wage-wage_l[i-1])/WAGE_STEP
-            break
-        history_poor[t]+=worker_l[i]
+    global history_poor,history_unemp,worker,unemp_n,unemp_e
+    n_people=0
+    for type in range(len(worker[0])):
+        for i in range(len(worker)):
+            for full_part in range(len(worker[0][0])):
+                n_people+=worker[i][type][full_part]
+        n_people+=unemp_n[type]+unemp_e[type]
+    history_unemp[t]=0
+    history_poor[t]=0
+    for type in range(len(worker[0])):
+        history_unemp[t]+=unemp_n[type]+unemp_e[type]
+        history_poor[t]+=unemp_n[type]+unemp_e[type]
+        for full_part in range(len(worker[0][0])):
+            for i in range(len(wage)):
+                if wage[i]*worktime[full_part]>min_liv_wage:
+                    history_poor[t]+=worker[i][type][full_part]*(min_liv_wage-wage[i-1]*worktime[full_part])/WAGE_STEP
+                    break
+                history_poor[t]+=worker[i][type][full_part]
+    history_unemp[t]=history_unemp[t]/n_people*100
+    history_poor[t]=history_poor[t]/n_people*100
 
-for t in range(TIME_LENGTH):#months
-    save_data(t)
-    if t%12==0:
-        update_unemp()
-        update_min_wage(t)
+for t in range(TIME_LENGTH):#years
+    #if t%12==0:
+    update_unemp()
+    update_min_wage(t)
     update_job()
     update_worker()
+    save_data(t)
 
 print("unemployed num:")
 print(history_unemp)
